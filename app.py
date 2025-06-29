b3a4d755150462650b618cbf436314d89c2fe30b
Vaisakh-Nirupam <vaisakhnirupamvn@gmail.com>
2025-06-29 10:00:20 +0530

    Database Connected
    - Created a connection with mysql
    - Created a table with required fields

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # App initialization
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:pass@localhost:3307/test_data"
+db = SQLAlchemy(app)
+
+# Table creation
+class TaskManager(db.Model):
+    sno = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    task = db.Column(db.String(200), nullable=True)
+    description = db.Column(db.String(500), nullable=True)
+    created = db.Column(db.Date, default=lambda:datetime.utcnow().date())
+    completed = db.Column(db.String(10), default="Pending")
+
+    def __repr__(self):
+        return f"{self.sno}. {self.task}"
 
 # Initial Path
 @app.route("/")
