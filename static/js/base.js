f4d4b1e991a380705787042a150c4d593d193982
Vaisakh-Nirupam <vaisakhnirupamvn@gmail.com>
2025-06-30 21:50:57 +0530

    Profile Page Complete
    - Displayed user details
    - Allows option to edit details
    - Added change password option
    - Added the js script so that the flash message timer takes the place of the button and after timer the button takes back its position.

--- static/js/base.js
+++ static/js/base.js
@@ -8,6 +8,23 @@ setTimeout(() => {
 }, 3000);
 
 
+// Fading the Flash Messages (& Hiding the Edit button on profile page) 
+const flash_profile = document.getElementById("flash-message-profile");
+const editButton = document.getElementById("edit_profile_btn");
+
+if (flash_profile && editButton) {
+    editButton.style.display = "none";
+    setTimeout(() => {
+        flash_profile.style.opacity = 0;
+        setTimeout(() => {
+            flash_profile.remove();
+            editButton.style.display = "inline-block";
+        }, 500);
+    }, 3000);
+}
+
+
+// Back and Exit Functionality on Signup Page
 document.querySelectorAll(".return_button").forEach(button => {
     button.addEventListener("click", function() {
         const action = this.textContent.trim().toLowerCase();
