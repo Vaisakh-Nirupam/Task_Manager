7483f4b356f8adfb281da7849af7856f7e611eab
Vaisakh-Nirupam <vaisakhnirupamvn@gmail.com>
2025-07-10 14:21:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -455,5 +455,5 @@ def logout():
 
 # Main guard
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    # app.run(debug=True)
